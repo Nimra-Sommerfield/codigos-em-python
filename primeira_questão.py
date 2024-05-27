46660f9a9d9e3e78b885f6a781d1c78cd81e30e0
Alison Bruno <161540994+Nimra-Sommerfield@users.noreply.github.com>
2024-05-27 14:50:03 -0300

    Create primeira_questão.py

---
+++ primeira_questão.py
@@ -0,0 +1,6 @@
+preco = float(input("Insira preço aqui:"))
+
+desconto = preco - (preco * 0.12)
+acrescimo = preco + (preco * 0.15)
+
+print(f"O preço com desconto é {desconto} e com acréscimo é {acrescimo}")
