45596da9b389eabe9914bcfb8fa18b0e610b5266
Alison Bruno <161540994+Nimra-Sommerfield@users.noreply.github.com>
2024-05-27 15:16:59 -0300

    Create piscina.py

---
+++ piscina.py
@@ -0,0 +1,9 @@
+#Cada metro cúbico é equivalente a 1000 litros
+#a cada mil litros 1.5 ml de produto é posto
+
+area = 1.7 * 3 * 5
+#já está em metros cubicos
+
+ml = area * 1.5
+
+print(f"{ml} mL")
